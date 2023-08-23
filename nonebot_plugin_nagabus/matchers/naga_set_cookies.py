d0ae5ccd5a346466a82676cafaadeae5f76ed238
ssttkkl <huang.wen.long@hotmail.com>
2023-08-24 01:32:14 +0800

    naga-set-cookies指令添加SUPERUSER权限及错误提示

--- nonebot_plugin_nagabus/matchers/naga_set_cookies.py
+++ nonebot_plugin_nagabus/matchers/naga_set_cookies.py
@@ -1,6 +1,7 @@
 from nonebot import on_command
 from nonebot.internal.matcher import Matcher
 from nonebot.params import CommandArg
+from nonebot.permission import SUPERUSER
 
 from nonebot_plugin_nagabus import default_cmd_start
 from nonebot_plugin_nagabus.ac import ac
@@ -9,20 +10,25 @@ from nonebot_plugin_nagabus.naga import naga
 
 set_token_srv = ac.create_subservice("set_cookies")
 
-set_token_matcher = on_command("naga-set-cookies", priority=4, block=True)
+set_token_matcher = on_command("naga-set-cookies", priority=4, block=True, permission=SUPERUSER)
 set_token_srv.patch_matcher(set_token_matcher)
 
 
 @set_token_matcher.handle()
 @handle_error()
 async def naga_set_cookies(matcher: Matcher, cmd_args=CommandArg()):
-    cookies = dict([
-        tuple(x.strip().split("=", maxsplit=1))
-        for x in cmd_args.extract_plain_text().split(";")
-    ])
+    try:
+        cookies = dict([
+            tuple(x.strip().split("=", maxsplit=1))
+            for x in cmd_args.extract_plain_text().split(";")
+        ])
 
-    if "csrftoken" not in cookies or "naga-report-session-id" not in cookies:
-        await matcher.finish(f"格式：{default_cmd_start}naga-set-cookies csrftoken=xxxxxxx; naga-report-session-id=xxxxxxx")
+        if "csrftoken" not in cookies or "naga-report-session-id" not in cookies:
+            raise ValueError("cookies must contain csrftoken and naga-report-session-id")
+    except ValueError:
+        await matcher.send(
+            f"格式：{default_cmd_start}naga-set-cookies csrftoken=xxxxxxx; naga-report-session-id=xxxxxxx")
+        return
 
     await naga.set_cookies(cookies)
     await matcher.send("设置成功")
